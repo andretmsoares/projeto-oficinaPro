99bbf279a46dbd390d0dd4b4a2c53eb37b71e72a
AndreTharssysMarquesSoares <andre.tharssys.marques.soares@ccc.ufcg.edu.br>
2026-08-21 15:15:38 -0300

    chore: project init

---
+++ src/main/java/com/oficinaPro/OficinaProApplication.java
@@ -0,0 +1,13 @@
+package com.oficinaPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oficinaPro/OficinaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oficinaPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OficinaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
