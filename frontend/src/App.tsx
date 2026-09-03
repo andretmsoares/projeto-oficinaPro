bd47712075912041ce51c7acde63ee45ff4b9ddc
andretmsoares <andre.tharssys.marques.soares@ccc.ufcg.edu.br>
2026-09-03 15:46:52 -0300

    fix: page login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout";
 import { Dashboard } from "./pages/Dashboard";
+import { Login } from "./pages/Login"; 
+import type { JSX } from "react/jsx-runtime";
+import { useState } from "react";
+
+function PrivateRoute({ children }: { children: JSX.Element }) {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" replace />;
+}
 
 function App() {
+  // Mude para true se quiser testar o Dashboard direto
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  if (!isAuthenticated) {
+    return <Login onLogin={() => setIsAuthenticated(true)} />;
+  }
+
   return (
-    <MainLayout>
+    <MainLayout onLogout={() => setIsAuthenticated(false)}>
       <Dashboard />
     </MainLayout>
   );
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,8 +1,27 @@
 import { LoginForm } from "../../components/LoginForm";
 
-export function Login() {
+import "./login.style.css";
+interface LoginProps {
+  onLogin: () => void;
+}
+
+export function Login({ onLogin }: LoginProps) {
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    // Simula validação e redireciona
+    onLogin();
+  }
+
+  return (
     <div className="login">
-        <img src='/logo.png' alt='Oficina Pro' />
-        <LoginForm />
+        <div className="login-container" onSubmit={handleSubmit}>
+            <img className="logo" src='/logo.png' alt='Oficina Pro' />
+            <div className="login-header"> 
+                <h1 className="login-title">Seja Bem vindo</h1>
+                <p className="login-subtitle">Faça login para continuar</p>
+            </div>
+            <LoginForm />
+        </div>
     </div>
+  );
 }
