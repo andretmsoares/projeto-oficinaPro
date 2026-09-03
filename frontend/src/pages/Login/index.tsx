f5330f08f74acded11f40bda899cdf02fefaafd0
andretmsoares <andre.tharssys.marques.soares@ccc.ufcg.edu.br>
2026-09-03 15:02:30 -0300

    feat: add favicon and logo

---
+++ frontend/src/pages/Login/index.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "../../components/LoginForm";
+
+export function Login() {
+    <div className="login">
+        <img src='/logo.png' alt='Oficina Pro' />
+        <LoginForm />
+    </div>
+}
